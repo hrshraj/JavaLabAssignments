2fc9d12f0dbc1596b3f0bf98fbf744786c615063
hrshraj <74167147+hrshraj@users.noreply.github.com>
2024-11-23 01:41:07 +0530

    Add files via upload

---
+++ Lab4/PatternRange.java
@@ -0,0 +1,29 @@
+package gsv.lab4;
+
+//Question5
+
+import java.util.Scanner;
+
+public class PatternRange {
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter range (a b):");
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		scanner.nextLine();
+		
+		System.out.println("Enter patter");
+		String pattern = scanner.nextLine();
+		
+		int count=0;
+		for(int i=a;i<=b;i++)
+		{
+			if(String.valueOf(i).contains(pattern))
+			{
+				count++;
+			}
+		}
+		System.out.println("The number of times "+pattern+" ocurred between "+a+" "+b+": "+count);
+	}
+}
